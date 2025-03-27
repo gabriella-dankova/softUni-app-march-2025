a23be9b148fe520b16009c6bf60d7f161fe4790c
gabriella-dankova <gabriella.dankova123@gmail.com>
2025-03-27 21:46:31 +0200

    creating catalog

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router'
 
-import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
 import Contacts from './components/Contacts'
 import Pricing from './components/Pricing'
 import NotFound from './components/NotFound'
+import Catalog from './components/Catalog'
+import './App.css'
 
 
 
@@ -17,7 +18,7 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/catalog' element={<h1>Catalog</h1>}/>
+        <Route path='/catalog' element={<Catalog />}/>
         <Route path='/contacts' element={<Contacts />}/>
         <Route path='/pricing' element={<Pricing />}/>
         <Route path='*' element={<NotFound />}/>
